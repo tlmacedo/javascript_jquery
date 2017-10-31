0f3fa80bc363dbce975e7f8b2ed8897d380c0210
Thiago Macedo <tl.macedo@hotmail.com>
2017-10-31 19:17:54 -0400

    auto-commit

---
+++ c02/js/string-variable.js
@@ -0,0 +1,9 @@
+var username;
+var message;
+username = 'Thiago';
+message = 'See our upcomming range';
+
+var elName = document.getElementById('name');
+elName.textContent = username;
+var elNote = document.getElementById('note');
+elNote.textContent = message;
